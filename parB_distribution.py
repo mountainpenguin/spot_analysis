a7bd4dafaa3827398f6cf94f1eb7663a15299a7b
mountainpenguin <pinguino.de.montana@googlemail.com>
2016-11-24 10:49:52 +0000

    parB_distribution: New script for plotting ParB distribution at birth (at least initially)

---
+++ parB_distribution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+""" Script to plot ParB distribution at birth
+
+Bins cells according to how many ParB foci they are born with
+Plots ParB position as percentage of cell length for each bin
+"""
+
+if __name__ == "__main__":
+    pass
